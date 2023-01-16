43741fabdaabf33f82977fd8cfc2b87a09165358
adjiroblox <mnameyou@gmail.com>
2023-01-16 15:57:00 +0800

    some update

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,25 @@
 // to clear all the screen     printf("\033[2J\033[H");
 // clear  screen     printf('\033[2J');
 
+char alloveragain[47] = "press s to start over again, press x to quit.\n";
+
+int alloveragains() {
+    char option;
+
+    option = getch();
+
+    if(option == 's')
+        return 0;
+    else if(option == 'x')
+        EXIT_FAILURE;
+    else {
+        printf("type better noob\n");
+        alloveragains();
+    }
+
+    return 0;
+}
+
 void bash() {
     char buffer[100];
 
@@ -34,10 +53,16 @@ void bash() {
 int main() {
     char buffer[100];
 
-    printf("\033[H\033[2J");
+    printf("\033[H\033[2J%s", alloveragain);
 
     for(int i = 0; i < sizeof(buffer); i++) {
         buffer[i] = getch();
+
+        if(buffer[i] == 127) {
+            buffer[i] = 0;
+            buffer[i - 1] = 0;
+            i = i - 2;
+        }
 
         if(buffer[i] == 10) {
             buffer[i] = 0;
@@ -46,11 +71,15 @@ int main() {
                 bash();
             }
 
-            return 0;
+            i = sizeof(buffer);
         }
     }
 
-    printf("you typed: %s", buffer);
+    printf("you typed: %s\n", buffer);
+
+    if(alloveragains() == 0) {
+        main();
+    } 
 
     return 0;
 }
--- test/test.cpp
+++ test/test.cpp
@@ -1,13 +1,37 @@
 #include <iostream>
+#include <conio.h>
+#include <string.h>
 
 // 
 //    \033[H: It moves the cursor at the top left corner of the screen or console.
 //   \033[2J: It clears the screen from the cursor to the end of the screen.
 //
 
-int main() {
+void fard() {
     //printf("\033[h");
     printf("\033[2J");
+}
+
+void backspacebruh() {
+    char b[100];
+
+    for(int i = 0; i < sizeof(b); i++) {
+        b[i] = getch();
+
+        if(b[i] == 10) {
+            i = sizeof(b);
+        }
+    }
+
+    printf("you typed: ");
+
+    for(int i = 0; i < strlen(b); i++) {
+        printf("%d", b[i]);
+    }
+}
+
+int main() {
+    backspacebruh();
 
     return 0;
 }
